48c2b2bfa8e7e7c1a5857f2a25b48ee34264d3af
quantiumtech <33174613+quantiumtech@users.noreply.github.com>
2023-11-27 04:35:09 -0800

    Added error logging

--- validation.js
+++ validation.js
@@ -1,4 +1,23 @@
+let form_errors = [];
+
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const questionsInput = document.getElementById('questions');
+const re = /^[A-Za-z0-9 .,!?@']+$/;
+
+function _pushError(type, errorMsg) {
+    form_errors.push({
+        field: type.id,
+        error: errorMsg
+    });
+
+}
+
 function _invalidInput(inputField, errorID) {
+    
+    _pushError(inputField, 'Illegal character detected.')
+
+
     inputField.classList.add('flash');
     document.getElementById(errorID).textContent = 'Illegal character detected.';
     setTimeout(() => {
@@ -15,13 +34,6 @@ function _invalidInput(inputField, errorID) {
         document.getElementById(errorID).textContent = '';
     }, 2200);
 }
-
-
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const questionsInput = document.getElementById('questions');
-const re = /^[A-Za-z0-9 .,!?']+$/;
-
 
 
 function _lengthCheck(inputField, limit) {
@@ -44,11 +56,31 @@ function _lengthCheck(inputField, limit) {
 
 }
 
-questionsInput.addEventListener('input', function() {
+// Store the last valid value of each input
+let lastValid = {
+    name: nameInput.value,
+    email: emailInput.value,
+    questions: questionsInput.value
+};
+
+function handleInput(inputField, fieldName) {
+    if (inputField.value.length <= 0 ||re.test(inputField.value)) {
+        lastValid[fieldName] = inputField.value; // Update the last valid value
+    } else {
+        _invalidInput(inputField, inputField.id + 'Error');
+        inputField.value = lastValid[fieldName]; // Revert to the last valid value
+    }
+}
+
+nameInput.addEventListener('input', () => handleInput(nameInput, 'name'));
+emailInput.addEventListener('input', () => handleInput(emailInput, 'email'));
+questionsInput.addEventListener('input', () => {
+    handleInput(questionsInput, 'questions');
     _lengthCheck(questionsInput, 250);
 });
 
-    _lengthCheck(questionsInput, 250);
+_lengthCheck(questionsInput, 250); // Initial check
+
 
 [nameInput, emailInput, questionsInput].forEach(function(inputField) {
     inputField.addEventListener('input', function(event) {
@@ -76,21 +108,29 @@ questionsInput.addEventListener('input', function() {
         document.getElementById('questionsError').textContent = '';
 
         if (!nameInput.checkValidity()) {
+            _pushError(nameInput, 'Please enter your name.')
             document.getElementById('nameError').textContent = 'Please enter your name.';
             isValid = false;
         }
 
         if (!emailInput.checkValidity()) {
+            _pushError(emailInput, 'Please enter a valid email address.')
             document.getElementById('emailError').textContent = 'Please enter a valid email address.';
             isValid = false;
         }
 
         if (questionsInput.value.length < 10 || questionsInput.value.length > 250) {
+            _pushError(questionsInput, 'Please enter between 10 and 250 characters for comments.')
             document.getElementById('questionsError').textContent = 'Please enter between 10 and 250 characters for comments.';
             isValid = false;
         } 
 
         if (!isValid) {
             event.preventDefault();
+            const errorInput = document.createElement('input');
+            errorInput.type = 'hidden';
+            errorInput.name = 'form_errors';
+            errorInput.value = JSON.stringify(form_errors);
+            this.appendChild(errorInput);
         }
     });
